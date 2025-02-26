dbae1e0e7e633654c9ff8b5a7cd5f413e99ca168
Sampreet Patil <sampreetpatil270@gmail.com>
2025-02-26 23:49:46 +0530

    Update, Draw sections in the main.cpp itself, frame rate set to 60

--- main.cpp
+++ main.cpp
@@ -1,20 +1,35 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // The below is the constructor
+    RenderWindow window(VideoMode({ 640, 400 }), "SFML works!", Style::Default);
+
+    window.setFramerateLimit(60); // This sets the framerate of the window
+    CircleShape shape(50.f);
+    shape.setFillColor(Color::White);
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
+            if (event->is<Event::Closed>())
                 window.close();
         }
 
+
+        // Update: usually we take keystrokes or updates from the user and draw the updated stuff in the draw section.
+        //shape.move(0.3f, 0.f);
+
+        
+
+        // Draw
         window.clear();
+
+        // Draw everything
         window.draw(shape);
         window.display();
     }
